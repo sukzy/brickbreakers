f77d03b3f3085a22b407cec7bfaab3e608fa0b60
sukzy <sukzy.mi@gmail.com>
2019-01-30 21:32:08 +0700

    update

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var brickRow = 2;
 let col = 0;
 let slotVer = 0;
 let slotHor = 0;
-let brickLength = 30;
+let brickLength = 20;
 
 function setup() {
 	createCanvas(700, 700);
@@ -23,7 +23,7 @@ function setup() {
 			col = 0;
 			bricks[i] = new Brick(slotHor, slotVer);
 			slotHor = slotHor + 70;
-		} 
+		}
 		col += 1;
 	}
 }
@@ -38,12 +38,17 @@ function draw() {
 	ball.show();
 	ball.update();
 	ball.checkEdge();
-	if(ball.hitPaddle(paddle)) {
-		ball.bounce();
+	if (ball.hitPaddle(paddle)) {
+		ball.bounceY();
 	}
 
 	for (let i = 0; i < bricks.length; i++) {
 		bricks[i].show();
+
+		if (ball.hitBrick(bricks[i])) {
+			bricks.splice(i, 1);
+			ball.bounceY();
+		}
 	}
 }
 
